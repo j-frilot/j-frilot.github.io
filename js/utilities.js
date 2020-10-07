481ddd749d86247b64a5929da8ec5c42fec67670
j-frilot <frilotdr@gmail.com>
2020-10-07 01:20:40 -0500

    added js utilities and redesigned form

---
+++ js/utilities.js
@@ -0,0 +1,47 @@
+///////////// dropdown MENU///////
+document.addEventListener("DOMContentLoaded", function () {
+  var drop = document.querySelectorAll(".dropdown-trigger");
+  var instances = M.Dropdown.init(drop, {});
+});
+
+///////MODALS////////
+document.addEventListener("DOMContentLoaded", function () {
+  var picOne = document.querySelectorAll(".modal");
+  M.Modal.init(picOne, {});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var picTwo = document.querySelectorAll(".modal");
+  M.Modal.init(picTwo, {});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var picThree = document.querySelectorAll(".modal");
+  M.Modal.init(picThree, {});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var picFour = document.querySelectorAll(".modal");
+  M.Modal.init(picFour, {});
+});
+
+// ///////TOOLTIPS////////
+document.addEventListener("DOMContentLoaded", function () {
+  var toolOne = document.querySelectorAll(".tooltipped");
+  var instances = M.Tooltip.init(toolOne, {});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var toolTwo = document.querySelectorAll(".tooltipped");
+  var instances = M.Tooltip.init(toolTwo, {});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var toolThree = document.querySelectorAll(".tooltipped");
+  var instances = M.Tooltip.init(toolThree, {});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var toolFour = document.querySelectorAll(".tooltipped");
+  var instances = M.Tooltip.init(toolFour, {});
+});
