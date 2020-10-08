426534ef005548c17df14d2579202eb6febd2f3c
j-frilot <frilotdr@gmail.com>
2020-10-08 16:08:51 -0500

    new

--- js/utilities.js
+++ js/utilities.js
@@ -4,44 +4,50 @@ document.addEventListener("DOMContentLoaded", function () {
   var instances = M.Dropdown.init(drop, {});
 });
 
+// ////////  WORK TABS  //////
+document.addEventListener("DOMContentLoaded", function () {
+  var workTab = document.querySelector(".tabs");
+  M.Tabs.init(workTab, {});
+});
+
 ///////MODALS////////
-document.addEventListener("DOMContentLoaded", function () {
-  var picOne = document.querySelectorAll(".modal");
-  M.Modal.init(picOne, {});
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var picOne = document.querySelectorAll(".modal");
+//   M.Modal.init(picOne, {});
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var picTwo = document.querySelectorAll(".modal");
-  M.Modal.init(picTwo, {});
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var picTwo = document.querySelectorAll(".modal");
+//   M.Modal.init(picTwo, {});
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var picThree = document.querySelectorAll(".modal");
-  M.Modal.init(picThree, {});
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var picThree = document.querySelectorAll(".modal");
+//   M.Modal.init(picThree, {});
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var picFour = document.querySelectorAll(".modal");
-  M.Modal.init(picFour, {});
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var picFour = document.querySelectorAll(".modal");
+//   M.Modal.init(picFour, {});
+// });
 
-// ///////TOOLTIPS////////
-document.addEventListener("DOMContentLoaded", function () {
-  var toolOne = document.querySelectorAll(".tooltipped");
-  var instances = M.Tooltip.init(toolOne, {});
-});
+// // ///////TOOLTIPS////////
+// document.addEventListener("DOMContentLoaded", function () {
+//   var toolOne = document.querySelectorAll(".tooltipped");
+//   var instances = M.Tooltip.init(toolOne, {});
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var toolTwo = document.querySelectorAll(".tooltipped");
-  var instances = M.Tooltip.init(toolTwo, {});
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var toolTwo = document.querySelectorAll(".tooltipped");
+//   var instances = M.Tooltip.init(toolTwo, {});
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var toolThree = document.querySelectorAll(".tooltipped");
-  var instances = M.Tooltip.init(toolThree, {});
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var toolThree = document.querySelectorAll(".tooltipped");
+//   var instances = M.Tooltip.init(toolThree, {});
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var toolFour = document.querySelectorAll(".tooltipped");
-  var instances = M.Tooltip.init(toolFour, {});
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var toolFour = document.querySelectorAll(".tooltipped");
+//   var instances = M.Tooltip.init(toolFour, {});
+// });
