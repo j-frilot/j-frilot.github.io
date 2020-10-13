2d4abf82d2089747e62225a5c72e224c98d04056
j-frilot <frilotdr@gmail.com>
2020-10-12 23:50:06 -0500

    added nav menu

--- js/utilities.js
+++ js/utilities.js
@@ -1,8 +1,8 @@
 ///////////// dropdown MENU///////
-document.addEventListener("DOMContentLoaded", function () {
-  var drop = document.querySelectorAll(".dropdown-trigger");
-  var instances = M.Dropdown.init(drop, {});
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var drop = document.querySelectorAll(".dropdown-trigger");
+//   var instances = M.Dropdown.init(drop, {});
+// });
 
 // /////////  TOOL TIP  ///////////
 
