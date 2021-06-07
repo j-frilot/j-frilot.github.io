50b15b58c4ee47a8c674d895e1dadcc595f40aa3
j-frilot <frilotdr@gmail.com>
2021-06-07 18:30:27 -0500

    added CPR Training Website to portfolio

--- js/utilities.js
+++ js/utilities.js
@@ -5,17 +5,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
 let projects = [
     {
+        img: "https://res.cloudinary.com/dsip2gx3i/image/upload/c_scale,h_300,q_auto/v1623107920/portfolio/work-calebras.jpg",
+        title: "Calebra's Heart CPR Training LLC",
+        link: "https://calebrasheart.netlify.app/"
+    },
+    {
         img: "https://res.cloudinary.com/dsip2gx3i/image/upload/c_scale,h_300,q_auto/v1619810176/portfolio/work-hospital-desktop.jpg",
-        title: "COVID Hospital",
+        title: "COVID General Hospital",
         link: "https://generalhospital.netlify.app/",
         code: "https://github.com/j-frilot/General-Hospital"
     },
     {
         img: "https://res.cloudinary.com/dsip2gx3i/image/upload/c_scale,h_300,q_auto/v1619809904/portfolio/work-paws.jpg",
         title: "Paws Pet Shop",
-        link: "https://paws-adoption.netlify.app/",
+        link: "https://petshop-jf.netlify.app/",
         code: "https://github.com/j-frilot/Paws-Pet-Shop"
     },
+
     {
         img: "https://res.cloudinary.com/dsip2gx3i/image/upload/c_scale,h_300,q_auto/v1611618803/math-game/thumbnail-phone.jpg",
         title: "Math Learning App",
@@ -26,26 +32,20 @@ let projects = [
     {
         img: "https://res.cloudinary.com/dsip2gx3i/image/upload/c_scale,h_300,q_auto/v1609289011/portfolio/work-pinterest.jpg",
         title: "Pinterest Clone",
-        link: "https://not-pinterest.netlify.app/",
+        link: "https://sports-pinterest.netlify.app/",
         code: "https://github.com/j-frilot/Pinterest-Clone"
     },
 
     {
         img: "https://res.cloudinary.com/dsip2gx3i/image/upload/c_scale,h_300,q_auto/v1609287683/portfolio/work-rockpaperscissors.jpg",
         title: "Rock, Paper, Scissors",
-        link: "https://j-frilot.github.io/Rock-Paper-Scissor",
+        link: "https://rockpaperscissor-jf.netlify.app/",
         code: "https://github.com/j-frilot/Rock-Paper-Scissor"
-    },
-    {
-        img: "https://res.cloudinary.com/dsip2gx3i/image/upload/c_scale,h_300,q_auto/v1609288913/portfolio/work-random-quote.jpg",
-        title: "Random Quote Maker",
-        link: "https://j-frilot.github.io/Random-Quote-Generator-/",
-        code: "https://github.com/j-frilot/Random-Quote-Generator-"
     },
     {
         img: "https://res.cloudinary.com/dsip2gx3i/image/upload/c_scale,h_300,q_auto/v1609288802/portfolio/work-groupme.jpg",
         title: "GroupMe Mobile App",
-        link: "https://j-frilot.github.io/GroupMe/",
+        link: "https://groupchat-jf.netlify.app/",
         code: "https://github.com/j-frilot/GroupMe"
     }
 ];
